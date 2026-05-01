c47033c49c075efd2db52d8dce3979e25e4ce591
KevinBonkoungou <bonkoungoukevin34@gmail.com>
2026-05-01 18:45:53 +0000

    RPS: invitation + confirmation + manches + gage vainqueur

--- app/webrtc.py
+++ app/webrtc.py
@@ -132,3 +132,53 @@ def handle_av_send(data):
         'type': data['type'],
         'challenge': data['challenge']
     }, room=f'user_{data["to_user"]}')
+
+# ============ RPS AVEC INVITATION ============
+rps_games = {}
+
+@socketio.on('rps_invite')
+def handle_rps_invite(data):
+    emit('rps_invite_received', {
+        'manches': data['manches'],
+        'gage': data['gage']
+    }, room=f'user_{data["to_user"]}')
+
+@socketio.on('rps_accept')
+def handle_rps_accept(data):
+    match_id = data['match_id']
+    rps_games[match_id] = {
+        'manches': data['manches'],
+        'gage': data['gage'],
+        'scores': {current_user.id: 0},
+        'choices': {},
+        'current_manche': 0
+    }
+    emit('rps_accepted', {'manches': data['manches'], 'gage': data['gage']}, room=f'user_{data["to_user"]}')
+
+@socketio.on('rps_decline')
+def handle_rps_decline(data):
+    emit('rps_declined', {}, room=f'user_{data["to_user"]}')
+
+@socketio.on('rps_round_choice')
+def handle_rps_choice(data):
+    match_id = data['match_id']
+    choice = data['choice']
+    
+    if match_id not in rps_games:
+        rps_games[match_id] = {'choices': {}}
+    
+    rps_games[match_id]['choices'][current_user.id] = choice
+    
+    if len(rps_games[match_id]['choices']) >= 2:
+        p1, p2 = list(rps_games[match_id]['choices'].keys())[:2]
+        c1, c2 = rps_games[match_id]['choices'][p1], rps_games[match_id]['choices'][p2]
+        
+        if c1 == c2: winner = 'draw'
+        elif (c1=='rock' and c2=='scissors') or (c1=='paper' and c2=='rock') or (c1=='scissors' and c2=='paper'): winner = p1
+        else: winner = p2
+        
+        # Envoyer résultat à chaque joueur
+        emit('rps_round_result', {'opponent_choice': c2, 'winner': winner}, room=f'user_{p1}')
+        emit('rps_round_result', {'opponent_choice': c1, 'winner': winner}, room=f'user_{p2}')
+        
+        rps_games[match_id]['choices'] = {}
